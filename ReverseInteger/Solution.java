56d2dfc00cc3cd2ecf1dab1a5021811562989ebe
Deepanshu Gupta <deepanshu.gupta@mavq.com>
2022-03-14 17:11:04 +0530

    Reverse Integer

---
+++ ReverseInteger/Solution.java
@@ -0,0 +1,14 @@
+package ReverseInteger;
+
+public class Solution {
+        public int reverse(int x) {
+            long d=0,sum=0;
+            while(x!=0){
+                d = x%10;
+                sum = (sum*10)+d;
+                x /= 10;
+            }
+            if(sum > Integer.MAX_VALUE || sum < Integer.MIN_VALUE) return 0;
+            return (int)sum;
+        }
+}
